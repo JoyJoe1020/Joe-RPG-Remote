f7f99cb94451fadceee91fda0f0665ede16f380d
HoldUpJoy <wcj979014958@hotmail.com>
2023-05-13 21:05:28 +0800

    更改FollowCamera中的部分代码，调整摄像机角度，实现和Disco Elysium中差不多的镜头效果

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -11,10 +11,16 @@ namespace RPG.Core
         // 定义一个Transform类型的变量target，用于存储跟随的目标对象
         [SerializeField] Transform target;
 
+        // 添加一个Vector3类型的变量offset，用于存储摄像机相对于目标的位置偏移
+        [SerializeField] Vector3 offset = new Vector3(0, 10, -10); 
+
         void Update()
         {
             // 将摄像机的位置设置为目标对象的位置，实现摄像机跟随目标的功能
             transform.position = target.position;
+
+            // 调整摄像机的角度，使其俯视目标
+            transform.LookAt(target);
         }
     }
 }
