c9a4c65abd0a1585a062c57cb9a087ae64eb8ebc
HoldUpJoy <wcj979014958@hotmail.com>
2023-05-18 16:03:17 +0800

    增加了玩家的传送功能。当玩家进入传送门时，不仅会切换到新的场景，还会将玩家移动到新场景中指定的出生点。
    具体实现方式是在Transition协程中，通过调用UpdatePlayer方法设置玩家的位置和旋转。在UpdatePlayer方法中，首先查找标签为"Player"的游戏对象，然后将玩家的位置和旋转设置为目标出生点（由spawnPoint指定）的位置和旋转。目标出生点是在另一个Portal对象中定义的，该对象通过GetOtherPortal方法获取。
    
    这种设计方式可以实现玩家在不同场景之间的无缝切换，提高了游戏的连贯性和沉浸感。

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,4 +1,5 @@
 ﻿// 引入所需的命名空间
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ namespace RPG.SceneManagement
     {
         // 用于在Inspector中设置要加载的场景的索引
         [SerializeField] int sceneToLoad = -1;
+        // 出生点的Transform组件，用于设置玩家在新场景中的位置和旋转
+        [SerializeField] Transform spawnPoint;
 
         // 当有其他物体进入该游戏对象的触发器时调用
         private void OnTriggerEnter(Collider other)
@@ -22,17 +25,60 @@ namespace RPG.SceneManagement
             }
         }
 
-        // 场景切换的协程
+        // 定义名为Transition的协程，用于处理场景的切换
         private IEnumerator Transition()
         {
-            // 在切换场景时保持此游戏对象不被销毁
+            // 调用DontDestroyOnLoad方法，该方法的功能是让指定的游戏对象在加载新场景时不被自动销毁
+            // 这里传入的是gameObject，表示当前的游戏对象，即包含Portal组件的游戏对象
             DontDestroyOnLoad(gameObject);
-            // 异步加载指定的场景，并等待加载完成
+
+            // 调用SceneManager.LoadSceneAsync方法，该方法的功能是异步加载指定的场景
+            // 这里传入的是sceneToLoad，表示要加载的场景的索引
+            // LoadSceneAsync方法会立即返回一个AsyncOperation对象，但是新场景的加载会在后台进行
+            // 新场景加载完成后，AsyncOperation的isDone属性会变为true
+            // 这里使用yield return将该AsyncOperation对象返回，这样可以在新场景加载完成前挂起协程的执行
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            // 当场景加载完成后，打印一条消息
-            print("Scene Loaded");
-            // 销毁此游戏对象
+
+            // 调用GetOtherPortal方法，获取目标场景的Portal对象
+            // GetOtherPortal方法的功能是在当前场景中查找所有的Portal对象，并返回第一个不是当前对象的Portal对象
+            Portal otherPortal = GetOtherPortal();
+
+            // 调用UpdatePlayer方法，更新玩家的位置和旋转
+            // UpdatePlayer方法的功能是查找标签为"Player"的游戏对象，然后将其位置和旋转设置为目标出生点的位置和旋转
+            UpdatePlayer(otherPortal);
+
+            // 调用Destroy方法，销毁当前的游戏对象
+            // 因为已经完成了场景切换和玩家的位置更新，所以这个Portal对象的任务已经完成，可以被销毁
             Destroy(gameObject);
+        }
+
+
+        // 更新玩家的位置和旋转
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            // 查找标签为"Player"的游戏对象
+            GameObject player = GameObject.FindWithTag("Player");
+            // 设置玩家的位置为目标出生点的位置
+            player.transform.position = otherPortal.spawnPoint.position;
+            // 设置玩家的旋转为目标出生点的旋转
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        // 获取目标场景的Portal对象
+        private Portal GetOtherPortal()
+        {
+            // 遍历当前场景中所有的Portal对象
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                // 如果是当前的Portal对象，跳过当前迭代
+                if (portal == this) continue;
+
+                // 返回找到的其他Portal对象
+                return portal;
+            }
+
+            // 如果没有找到其他的Portal对象，返回null
+            return null;
         }
     }
 }
