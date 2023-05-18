74e4c5fee10e046240bfeaea8fd3a1c9f8f63a5d
HoldUpJoy <wcj979014958@hotmail.com>
2023-05-18 15:33:58 +0800

    通过Unity的触发器系统（Trigger）和协程（Coroutine）实现场景的切换。当玩家的游戏对象进入Portal对象的触发区域时，OnTriggerEnter方法会被调用，启动Transition协程进行场景切换。在Transition协程中，首先调用DontDestroyOnLoad方法保证在切换场景时，Portal对象不会被销毁；然后通过SceneManager.LoadSceneAsync方法异步加载指定的场景，等待场景加载完成；加载完成后，打印一条消息，最后销毁Portal对象。防止游戏在加载大型场景时出现卡顿，提高用户体验。

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,20 +1,38 @@
-﻿using System.Collections;
+﻿// 引入所需的命名空间
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// 在RPG.SceneManagement命名空间下定义一个名为Portal的类，继承自MonoBehaviour
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        // 用于在Inspector中设置要加载的场景的索引
         [SerializeField] int sceneToLoad = -1;
 
-        private void OnTriggerEnter(Collider other) 
+        // 当有其他物体进入该游戏对象的触发器时调用
+        private void OnTriggerEnter(Collider other)
         {
+            // 如果进入触发器的物体标签为"Player"，则启动场景切换的协程
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        // 场景切换的协程
+        private IEnumerator Transition()
+        {
+            // 在切换场景时保持此游戏对象不被销毁
+            DontDestroyOnLoad(gameObject);
+            // 异步加载指定的场景，并等待加载完成
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            // 当场景加载完成后，打印一条消息
+            print("Scene Loaded");
+            // 销毁此游戏对象
+            Destroy(gameObject);
         }
     }
 }
