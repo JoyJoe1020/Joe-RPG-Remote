b499a9fbd796231d3a91f74a10bc0e93973f725e
HoldUpJoy <wcj979014958@hotmail.com>
2023-05-25 19:22:38 +0800

    再次修改音效

--- Assets/Scripts/UI/PauseMenuUI.cs
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -22,7 +22,7 @@ namespace RPG.UI
         private void OnDisable()
         {
             if (playerController == null) return;
-            Time.timeScale = 3;
+            Time.timeScale = 1;
             playerController.enabled = true;
         }
 
